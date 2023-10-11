90541d42858e14b0bb38ad34143028c2a4993afa
andrewtan2000 <andrew_tan2000@hotmail.com>
2023-10-11 23:53:36 +0800

    User Microservice to List Profile Updates on field values

--- src/main/resources/db.migration/V2__insert_data.sql
+++ src/main/resources/db.migration/V2__insert_data.sql
@@ -1,5 +1,5 @@
 INSERT INTO sjmsuser.user_profile (id,name,seeking,job_title,image,about)
 VALUES
-    (1, 'Andrew Tan', true, 'Assistant Principal Engineer', 'url', 'Job seeker with 10 years experience in programming'),
-    (2, 'Ernest Lee', true, 'Principal Engineer', 'url','Job seeker with 10 years experience in programming'),
-    (3, 'Wang Shiji', false, 'Senior Vice President', 'url','Job seeker with 10 years experience in programming');
+    (1, 'Andrew Tan', true, 'Production Support Engineer', 'url', 'Reduced the number of customer support tickets by 15% by developing a new knowledge base.'),
+    (2, 'Ernest Lee', true, 'Principal Engineer', 'url','Led a team of engineers in the development of a new mobile app that has been downloaded over 1 million times.'),
+    (3, 'Wang Shiji', false, 'Senior Data Scientist', 'url','Developed a new algorithm that improved the performance of our software by 20%.');
