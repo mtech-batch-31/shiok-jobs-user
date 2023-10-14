ff2c043a1544834fefe12da07f842eec4ff927d7
ernestjx <73307945+ernestjx@users.noreply.github.com>
2023-10-14 17:25:20 +0800

    updated Sns integration part

--- src/main/java/com/mtech/sjmsuser/util/InstantSerializer.java
+++ src/main/java/com/mtech/sjmsuser/util/InstantSerializer.java
@@ -10,6 +10,6 @@ import java.time.Instant;
 public class InstantSerializer extends JsonSerializer<Instant> {
     @Override
     public void serialize(Instant value, JsonGenerator gen, SerializerProvider serializers) throws IOException {
-        gen.writeString(String.valueOf(value.getEpochSecond()));
+        gen.writeString(String.valueOf(value.toEpochMilli()));
     }
 }
