9cbbe16b9425c21b56ea3b2a9f8dfbdbc4413448
ernestjx <73307945+ernestjx@users.noreply.github.com>
2024-03-10 14:00:26 +0800

    clean

--- src/main/java/com/mtech/sjmsuser/repository/EducationRepository.java
+++ src/main/java/com/mtech/sjmsuser/repository/EducationRepository.java
@@ -7,9 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.UUID;
 
 public interface EducationRepository extends JpaRepository<Education, UUID> {
-//    Optional<UserProfile> findById(long id);
 
-//    @Modifying
     int deleteByUserProfile(UserProfile userProfile);
 
 }
