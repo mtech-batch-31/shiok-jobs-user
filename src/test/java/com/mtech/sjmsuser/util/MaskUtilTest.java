5c0d839098ce70437f306be8f497d38f2278e40a
Htet Thura Zaw (Raymond) <htetthurazaw@gmail.com>
2024-03-25 21:27:27 +0800

    increase code coverage

---
+++ src/test/java/com/mtech/sjmsuser/util/MaskUtilTest.java
@@ -0,0 +1,20 @@
+package com.mtech.sjmsuser.util;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MaskUtilTest {
+
+    @Test
+    public void testMaskUtil_AbleToMaskResponse() {
+        String input = "{ \"name\": \"Test123\", \"email\": \"test@test.com\" }";
+
+        String maskedValue = MaskUtil.applyMask(input);
+
+        String expectedValue = "{\"name\":\"\",\"email\":\"\"}";
+
+        Assertions.assertEquals(expectedValue, maskedValue);
+
+        System.out.println(input);
+    }
+}
